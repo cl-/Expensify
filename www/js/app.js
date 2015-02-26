81bde01b454d5586c232e1ba07e0d7baa6475446
cl <cl@startup.org.sg>
2015-02-27 00:04:45 +0800

    restore part2

--- www/js/app.js
+++ www/js/app.js
@@ -47,6 +47,27 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
+
+  .state('app.playlists', {
+    url: "/playlists",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlists.html",
+        controller: 'PlaylistsCtrl'
+      }
+    }
+  })
+
+  .state('app.single', {
+    url: "/playlists/:playlistId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlist.html",
+        controller: 'PlaylistCtrl'
+      }
+    }
+  })
+
     .state('app.recommendations', {
       url: "/recommendations",
       views: {
@@ -67,5 +88,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
+  // $urlRouterProvider.otherwise('/app/playlists');
   $urlRouterProvider.otherwise('/app/recommendations');
+  
 });
